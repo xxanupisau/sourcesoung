ba839c51a6f73444655863d397c7727a4b05c55d
siriam <91399753+xxanupisau@users.noreply.github.com>
2021-12-15 09:15:56 +0200

    Add files via upload

---
+++ cache/__init__.py
@@ -0,0 +1,11 @@
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# Rocks © @bar_lo0o0 © Rocks
+# Owner BARLO
+
+from cache.admins import admins, get, set
+
+__all__ = ["admins", "get", "set"]
+
+
+# Noob
